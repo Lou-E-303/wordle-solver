4d76b088897a98ecbb9d2c3e2d3827a600e3d447
Lou-E-303 <louiscowell404@gmail.com>
2022-01-04 02:41:02 +0000

    Switched to using Wordle creator's dictionary

--- README.md
+++ README.md
@@ -3,7 +3,7 @@ Wordle Solver
 
 A program which tries to solve [Wordle](https://www.powerlanguage.co.uk/wordle/) puzzles as efficiently as possible.
 
-Dictionary text file found [here](https://github.com/dwyl/english-words).
+Dictionary text file found [here](https://raw.githubusercontent.com/powerlanguage/word-lists/master/word-list-raw.txt).
 
 
 
--- generate_five_letter_words.py
+++ generate_five_letter_words.py
@@ -1,5 +1,5 @@
 def load_words():
-    with open('words_alpha.txt') as word_file:
+    with open('word_list.txt') as word_file:
         valid_words = set(word_file.read().split())
 
     return valid_words
@@ -10,7 +10,6 @@ five_letter_words = open("five_letter_words.txt", "w+")
 
 for word in english_words:
     if len(word) == 5:
-        print("Wrote word: " + word)
         five_letter_words.write(word+"\n")
 
 five_letter_words.close()
