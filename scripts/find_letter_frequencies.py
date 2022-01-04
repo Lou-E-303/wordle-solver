cb9fcc35957974d961375d2a65eb6f49ac3f1216
Lou-E-303 <louiscowell404@gmail.com>
2022-01-04 14:27:25 +0000

    Output letter frequencies as JSON file

--- scripts/find_letter_frequencies.py
+++ scripts/find_letter_frequencies.py
@@ -1,7 +1,8 @@
 from load_words import load_words
 from collections import Counter
+import json
 
-five_letter_words = load_words('../resources/five_letter_words_one_line.txt')
+five_letter_words = load_words('resources/five_letter_words_one_line.txt')
 
 letter_frequencies = Counter(str(five_letter_words))
 
@@ -11,6 +12,13 @@ letter_frequencies.pop('\"')
 letter_frequencies.pop('{')
 letter_frequencies.pop('}')
 letter_frequencies.pop('/')
+
+letter_frequencies = letter_frequencies.most_common()
+
+with open('resources/letter_frequencies.json', 'w') as letter_frequencies_json:
+    json.dump(dict(letter_frequencies), letter_frequencies_json)
+
+print(letter_frequencies)
 
 # A, E, S, O, R are the most common letters
 # Therefore by this metric 'arose' should be our starting word
