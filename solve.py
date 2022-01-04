f111fed42770c7b810ac359671ddbc90859cc20d
Lou-E-303 <louiscowell404@gmail.com>
2022-01-04 11:51:09 +0000

    WIP: Replaced letter-state pair with index-state pair as dicts are inherently unordered

--- solve.py
+++ solve.py
@@ -1,7 +1,11 @@
-guess = 'arose'
+from scripts.load_words import load_words
 
-for i in range(1, 6):
+guess = 'arose'
+previous_guesses = []
+
+for i in range(1, 7):
     print("Try '" + guess + "'.")
+    previous_guesses.append(guess)
 
     acceptable_states = ['A', 'N', 'Y', 'G']
 
@@ -11,24 +15,34 @@ for i in range(1, 6):
     # G for Green (in word at this index)
 
     result = {
-        guess[0]: "A",
-        guess[1]: "A",
-        guess[2]: "A",
-        guess[3]: "A",
-        guess[4]: "A"
+        0: "A",
+        1: "A",
+        2: "A",
+        3: "A",
+        4: "A"
     }
 
     for j in range(0, 5):
         tile_state = input("What colour was the '" + guess[j] + "' tile? (N/Y/G)? ")
 
         if tile_state in acceptable_states:
-            result[guess[j]] = tile_state
+            result[j] = tile_state
         else:
             print("Some error message or other and retry - TODO")
 
-        # Now find all the words that fit the criteria:
-        # 1. They have the yellow letters in them somewhere
-        # 2. They have the green letters in the correct indices
-        # 3. They have not been guessed before
-        # And order them by letter frequency
-        # Finally set guess to the most likely guess
+    # Now find all the words that fit the criteria:
+    # 1. They have not been guessed before ✅
+    # 2. They have the green letters in the correct indices
+    # 3. They have the yellow letters in them somewhere
+    # And order them by letter frequency
+    # Finally set guess to the most likely guess
+
+#     possible_words = find_possible_words(result)
+#
+#
+# def find_possible_words(result):
+#     five_letter_words = load_words('../resources/five_letter_words.txt')
+#     for word in five_letter_words:
+#         if word in previous_guesses:
+#             five_letter_words.remove(word)
+#
