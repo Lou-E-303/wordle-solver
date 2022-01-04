fe37d3cbb3669a07cb471eeb7a9fe809bda6cd6d
Lou-E-303 <louiscowell404@gmail.com>
2022-01-04 17:14:02 +0000

    Removed punctuation from word list

---
+++ generate_five_letter_words.py
@@ -0,0 +1,21 @@
+import string
+
+from utils.load_all_words import load_all_words
+
+
+def word_contains_no_punctuation(word):
+    punctuation = string.punctuation
+    for i in range(0, len(punctuation)):
+        if punctuation[i] in word:
+            return False
+    return True
+
+
+english_words = load_all_words('resources/word_list.txt')
+five_letter_words = open("resources/five_letter_words.txt", "w+")
+
+for word in english_words:
+    if len(word) == 5 and word_contains_no_punctuation(word):
+        five_letter_words.write(word + "\n")
+
+five_letter_words.close()
