16d24a48f41916d58f164deae2da053f268a0fec
Lou-E-303 <louiscowell404@gmail.com>
2026-06-09 12:21:00 +0100

    Skip score processing logic if there is only one possible answer

--- src/utils/suggest_next_guess.py
+++ src/utils/suggest_next_guess.py
@@ -2,7 +2,11 @@ import sys
 
 def suggest_next_guess(possible_words, letter_frequencies, current_round):
     word_score_pairs = {}
-    duplicate_letter_penalty = 750 - (current_round * 100)
+    duplicate_letter_penalty = 750 - (current_round * 150)
+
+    possible_words = list(possible_words)
+    if len(possible_words) == 1:
+        return possible_words[0]
 
     for word in possible_words:
         score = 0
